5f5bdea0ace743f4b48e8e49b513d29776977d11
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-09-05 18:58:52 +0530

    Created Interview1 component

---
+++ src/Interview1.js
@@ -0,0 +1,21 @@
+
+function Interview1()
+{
+    return(<>
+  <div>
+    <h3>Interview </h3>
+    <p>console.log(5+'3'); Output is : {5+'3'}</p>
+    <p>console.log('3'+5); Output is : {'3'+5}</p>
+    <p>console.log(5-'3'); Output is : {5-'3'}</p>
+    <p>console.log(5*'3'); Output is : {5*'3'}</p>
+    <p>console.log(5%'3'); Output is : {5%'3'}</p>
+    <p>console.log(8/'2'); Output is : {8/'2'}</p>
+    <p>console.log(5==5); Output is : {String(5==5)}</p>
+    <p>console.log(5=='5'); Output is : {String(5=='5')}</p>
+    <p>console.log(5==='5'); Output is : {String(5==='5')}</p>
+    
+  </div>
+    </>);
+}
+export default Interview1;
+
